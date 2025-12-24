ebc7c50d3d126300912732fa4749d735233c5956
Sreejan <sreejan.naru@capsitech.com>
2025-12-24 16:24:02 +0530

    refactor: updated function to support invoice creation

--- scripts/businessflow.ts
+++ scripts/businessflow.ts
@@ -16,6 +16,8 @@ type QuoteData = {
   amount: number;
 };
 
+export type QuoteType = "Create" | "Renew" | "Activate" | "Expand";
+
 export async function enterBusinessPage(page: Page, number: string | number) {
   await logIn(page);
 
@@ -31,37 +33,48 @@ export async function enterBusinessPage(page: Page, number: string | number) {
   await expect(page.getByText("Meetings")).toBeVisible();
 }
 
-export async function createQuote(
+export async function createQuoteInvoice(
   page: Page,
-  quote: "Create" | "Renew" | "Activate" | "Expand",
+  quote: QuoteType,
+  isQuote: boolean,
+  createExists: boolean = true,
 ) {
-  // Go to Quotes tab
-  await page.getByRole("tab", { name: "Quotes" }).click();
+  // Go to the tab
+  await page
+    .getByRole("tab", { name: isQuote ? "Quotes" : "Invoices" })
+    .click();
+
+  const idStart = isQuote ? "#esit-quote" : "#update-subscription";
 
   await page.getByRole("button", { name: "Add New" }).click();
-  await page.locator("#esit-quote-form_planType").click();
-  if (quote == "Create") {
-    for (let i = 0; i < 4; i++) {
-      await page.keyboard.press("ArrowDown");
-    }
-    await page.keyboard.press("Enter");
-    await page.keyboard.press("Enter");
-  } else if (quote == "Activate") {
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.keyboard.press("Enter");
-  } else if (quote == "Renew") {
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-    await page.keyboard.press("Enter");
-  } else if (quote == "Expand") {
-    for (let i = 0; i < 3; i++) {
-      await page.keyboard.press("ArrowDown");
-    }
-    await page.keyboard.press("Enter");
-    await page.keyboard.press("Enter");
+  await page.locator(`${idStart}-form_planType`).click();
+
+  const defaultMap: Record<QuoteType, number> = {
+    Activate: 1,
+    Renew: 2,
+    Expand: 3,
+    Create: 4,
+  };
+  const noCreateMap: Partial<Record<QuoteType, number>> = {
+    Renew: 1,
+    Expand: 2,
+    Activate: 3,
+  };
+
+  const map = createExists ? defaultMap : noCreateMap;
+  const steps = map[quote];
+
+  if (steps === undefined) {
+    throw new Error(
+      `Quote option "${quote}" not available when createExists=${createExists}`,
+    );
   }
+
+  for (let i = 0; i < steps; i++) {
+    await page.keyboard.press("ArrowDown");
+  }
+  await page.keyboard.press("Enter");
+  await page.keyboard.press("Enter");
 
   const data: QuoteData = {
     date: getDateDDMMYYYY("past", 7),
@@ -72,24 +85,28 @@ export async function createQuote(
   };
 
   if (quote !== "Expand") {
-    await page.locator("#esit-quote-form_subscriptionPeriod").click();
+    await page.locator(`${idStart}-form_subscriptionPeriod`).click();
     // Subscription period
-    await page.locator("#esit-quote-form_subscriptionPeriod").fill(data.start);
+    await page.locator(`${idStart}-form_subscriptionPeriod`).fill(data.start);
     await page.keyboard.press("Enter");
     await page.keyboard.type(data.end);
     await page.keyboard.press("Enter");
   }
 
-  await page.locator("#esit-quote-form_empCount").fill(data.emp.toString());
-  await page.locator("#esit-quote-form_amount").fill(data.amount.toString());
+  await page.locator(`${idStart}-form_empCount`).fill(data.emp.toString());
+  await page.locator(`${idStart}-form_amount`).fill(data.amount.toString());
 
-  await page.locator("#esit-quote-form_date").click();
-  await page.locator("#esit-quote-form_date").fill(data.date);
+  await page.locator(`${idStart}-form_date`).click();
+  await page.locator(`${idStart}-form_date`).fill(data.date);
   await page.keyboard.press("Enter");
 
   await page.getByRole("button", { name: "Save" }).click();
 
-  await expect(page.getByText("Quote saved successfully")).toBeVisible();
+  await expect(
+    page.getByText(
+      `${isQuote ? "Quote saved" : "Invoice created"} successfully`,
+    ),
+  ).toBeVisible();
   return data;
 }
 
