24a925e0e5576711f6929c0e73d78aab308b29f7
Sreejan <sreejan.naru@capsitech.com>
2025-12-24 16:39:33 +0530

    feat: added auxilary functions for email related operations

--- utils/mail-reader.ts
+++ utils/mail-reader.ts
@@ -1,31 +1,33 @@
-import { Browser } from "@playwright/test";
+import { Browser, expect } from "@playwright/test";
+import { drawOnCanvas, verifyPaymentInfo } from "./helpers";
 
-export type TempMailOptions = {
-  username: string;
-  domain?: string;
-  otpLength?: number;
-};
+export async function openInbox(browser: Browser, options: TempMailOptions) {
+  const { username, domain = "chitthi.in" } = options;
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://tempmail.plus/en/#!");
+
+  // Set username
+  await page.locator("#pre_button").fill(username);
+
+  // Select domain
+  await page.locator("#domain").click();
+  await page.getByRole("button", { name: domain }).click();
+  await page.locator("#pre_copy").click();
+
+  return { page, context };
+}
 
 export async function readOtpFromTempmail(
   browser: Browser,
   options: TempMailOptions,
 ): Promise<string> {
-  const { username, domain = "chitthi.in", otpLength = 4 } = options;
-
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  const { page, context } = await openInbox(browser, options);
+  const { otpLength = 4 } = options;
 
   try {
-    await page.goto("https://tempmail.plus/en/#!");
-
-    // Set username
-    await page.locator("#pre_button").fill(username);
-
-    // Select domain
-    await page.locator("#domain").click();
-    await page.getByRole("button", { name: domain }).click();
-    await page.locator("#pre_copy").click();
-
     // Open latest email
     await page.getByText("Verification Code").click();
 
@@ -39,6 +41,46 @@ export async function readOtpFromTempmail(
     ).trim();
 
     return otp;
+  } finally {
+    await context.close();
+  }
+}
+
+export async function acceptQuoteTempmail(
+  browser: Browser,
+  options: TempMailOptions,
+  data: QuoteData,
+  quote: QuoteType,
+) {
+  const { page, context } = await openInbox(browser, options);
+  try {
+    // Open latest email
+    await page.getByText("Quotation for").first().click();
+
+    // --- CLICK + CAPTURE REDIRECT ---
+    const [newPage] = await Promise.allSettled([
+      context.waitForEvent("page"), // new tab
+      page.getByText("Accept Quotation").click({ force: true }),
+    ]);
+
+    // Determine where we landed
+    const quotePage = newPage.status === "fulfilled" ? newPage.value : page;
+
+    // Wait for final redirect page to load
+    await quotePage.waitForLoadState("domcontentloaded");
+
+    await verifyPaymentInfo(quotePage, quote, data, false, false);
+
+    // Accept the Quote
+    await quotePage.getByRole("button", { name: "Accept" }).click();
+
+    await drawOnCanvas(quotePage);
+
+    await quotePage.getByRole("button", { name: "Save" }).click();
+
+    await expect(
+      quotePage.getByText("Quote accepted successfully"),
+    ).toBeVisible();
   } finally {
     await context.close();
   }
