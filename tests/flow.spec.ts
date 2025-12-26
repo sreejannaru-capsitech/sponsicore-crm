313aebbde73fb9344ff7218a1fa6e205890afeaa
Sreejan <sreejan.naru@capsitech.com>
2025-12-26 17:24:59 +0530

    feat: added full flow test case

--- tests/flow.spec.ts
+++ tests/flow.spec.ts
@@ -8,45 +8,106 @@ import {
   updateFollowup,
   updateCallback,
   changeStatus,
+  createPortalLead,
 } from "../scripts/leadflow";
 import {
-  createQuote,
+  createQuoteInvoice,
   enterBusinessPage,
+  quoteToInvoice,
+  verifyInvoicePay,
   verifyQuoteCreation,
 } from "../scripts/businessflow";
+import { getUsername, logIn } from "../utils/helpers";
+import { trueFalse } from "../utils/generators";
+import { verifyClientCreation } from "../scripts/clientflow";
 
-test.describe("Create and Update Lead", () => {
-  test("Contact lead", async ({ page }) => {
-    const data = await createContactQueryLead(page);
-    await enterLeadPage(page, data);
-    await checkLeadHistory(page, "create");
+// test.describe("Create Leads from Website", () => {
+//   test("Contact lead", async ({ page }) => {
+//     const data = await createContactQueryLead(page);
+//     await logIn(page);
+//     await enterLeadPage(page, data);
+//     await checkLeadHistory(page, "create");
+//   });
 
-    // await updateFollowup(page);
-    // await updateCallback(page);
-    await changeStatus(page, "business");
+//   test("Pricing lead", async ({ page, browser }) => {
+//     const data = await createPricingQueryLead(page, browser);
+//     await logIn(page);
+//     await enterLeadPage(page, data.data);
+//     await checkLeadHistory(page, "create");
+//   });
+
+//   test("Compliance lead", async ({ page, browser }) => {
+//     const data = await createComplianceTestLead(page, browser);
+//     await logIn(page);
+//     await enterLeadPage(page, data);
+//     await checkLeadHistory(page, "create");
+//   });
+// });
+
+test.describe("Company creation from Lead", () => {
+  test.beforeEach(async ({ page }) => {
+    await logIn(page);
   });
 
-  test("Pricing lead", async ({ page, browser }) => {
-    const data = await createPricingQueryLead(page, browser);
-    await enterLeadPage(page, data.data);
+  // test("Test", async ({ page }) => {
+  //   await logIn(page);
+  // });
+
+  test("Quote Invoice Creation", async ({ page, browser }) => {
+    test.setTimeout(100_000);
+
+    const leadData = await createPortalLead(page);
+    await enterLeadPage(page, leadData);
     await checkLeadHistory(page, "create");
 
-    await updateFollowup(page);
-  });
-
-  test("Compliance lead", async ({ page, browser }) => {
-    const data = await createComplianceTestLead(page, browser);
-    await enterLeadPage(page, data);
-    await checkLeadHistory(page, "create");
-
-    await updateFollowup(page);
-  });
-});
-
-test.describe("Business update", () => {
-  test("Quote Creation", async ({ page, browser }) => {
-    await enterBusinessPage(page, "14145072");
-    const data = await createQuote(page, "Create");
+    const company = await changeStatus(page, "business");
+    await enterBusinessPage(page, company);
+    // creates quote
+    const data = await createQuoteInvoice(page, "Create", true);
     await verifyQuoteCreation(page, "Create", data);
+
+    const username = getUsername(leadData);
+
+    await quoteToInvoice(page, "Create", data, username, browser);
+    const makePayment = trueFalse();
+    await verifyInvoicePay(
+      page,
+      "Create",
+      { ...data, date: new Date().toLocaleDateString("en-GB") },
+      browser,
+      username,
+      trueFalse(),
+      makePayment,
+    );
+    if (makePayment) {
+      await verifyClientCreation(page, company);
+    }
+  });
+
+  test("Direct Invoice Creation", async ({ page, browser }) => {
+    test.setTimeout(100_000);
+
+    const leadData = await createPortalLead(page);
+    await enterLeadPage(page, leadData);
+    await checkLeadHistory(page, "create");
+
+    const company = await changeStatus(page, "business");
+    await enterBusinessPage(page, company);
+    // creates invoice
+    const data = await createQuoteInvoice(page, "Create", false);
+
+    const makePayment = trueFalse();
+    await verifyInvoicePay(
+      page,
+      "Create",
+      data,
+      browser,
+      getUsername(leadData),
+      trueFalse(),
+      makePayment,
+    );
+    if (makePayment) {
+      await verifyClientCreation(page, company);
+    }
   });
 });
